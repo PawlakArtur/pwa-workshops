44a937999402140fdd0a21448babfdb573454ca5
Artur Pawlak <pawlak.artur90@gmail.com>
2018-02-24 13:49:38 +0100

    Add delete old caches on 'activate' event

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,5 @@
 /* eslint no-restricted-globals: 0 */
-const cacheName = 'cache-v1';
+const cacheName = 'cache-v3';
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-undef
     const { assets } = serviceWorkerOption;
@@ -7,10 +7,17 @@ self.addEventListener('install', event => {
         caches.open(cacheName).then(function(cache) {
             return cache.addAll(assets);
           })
-    )
+    );
 });
 
 self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== cacheName)
+                    .map(key => caches.delete(key))
+            );
+        }));
 });
 
 self.addEventListener('fetch', event => {
