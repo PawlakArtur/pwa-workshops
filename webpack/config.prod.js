4c3efbf9dbad39729b3389c6843a7ec014f48488
Artur Pawlak <pawlak.artur90@gmail.com>
2018-02-24 13:41:15 +0100

    Excludes '_redirects' file in webpack config.prod

--- webpack/config.prod.js
+++ webpack/config.prod.js
@@ -117,6 +117,7 @@ module.exports = {
     new webpack.optimize.UglifyJsPlugin(),
     new ServiceWorkerWebpackPlugin({
       entry: path.join(__dirname, '../src/sw.js'),
+      excludes: ['_redirects'],
     }),
   ],
 };
